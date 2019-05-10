95090ea296f974cb7192ef7c2cc5ad3446190475
sth23 <47009471+sth23@users.noreply.github.com>
2019-05-10 15:02:27 -0400

    Updated from Brython Server: 5/10/2019 3:02:27 PM

--- missile-command-game.py
+++ missile-command-game.py
@@ -30,15 +30,13 @@ class Turret(Sprite):
         MissileCommandGame.listenKeyEvent("keyup", "right arrow", self.aimRightOff)
         
     def aimRightOn(self, event):
-        if self.rotation > 0:
-            self.vr = self.maxspin
+        self.vr = self.maxspin
             
     def aimRightOff(self, event):
         self.vr = 0
         
     def aimLeftOn(self, event):
-        if self.rotation < math.pi:
-            self.vr = -self.maxspin
+        self.vr = -self.maxspin
             
     def aimLeftOff(self, event):
         self.vr = 0
