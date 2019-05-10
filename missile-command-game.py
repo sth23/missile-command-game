603f14d4f7de78efd010ca78a22db2b8c7e6bffe
sth23 <47009471+sth23@users.noreply.github.com>
2019-05-10 16:13:32 -0400

    Updated from Brython Server: 5/10/2019 4:13:31 PM

--- missile-command-game.py
+++ missile-command-game.py
@@ -101,7 +101,7 @@ class Ground(Sprite):
     def __init__(self, width, height):
         self.gameheight = height
         self.gamewidth = width
-        super().__init__(RectangleAsset(self.gamewidth, 60), Ground.noline, .Ground.black)
+        super().__init__(RectangleAsset(self.gamewidth, 60, Ground.noline, Ground.black), (0, 0))
 
 class MissileTail(Sprite):
     # Create asset
