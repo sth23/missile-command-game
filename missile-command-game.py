996d9da611bd6fb3c62c0c6b806501aa483d60c5
sth23 <47009471+sth23@users.noreply.github.com>
2019-05-10 10:06:39 -0400

    Updated from Brython Server: 5/10/2019 10:06:39 AM

--- missile-command-game.py
+++ missile-command-game.py
@@ -42,13 +42,9 @@ class MissileHead(Sprite):
         self.gamewidth = width
         self.gameheight = height
         self.fxcenter = self.fycenter = 0.5
-        """
         self.theta1 = (math.atan2(self.gameheight, self.x) / math.pi)
         self.theta2 = (math.pi - math.atan2(self.gameheight, self.gamewidth - self.x)) / math.pi
-        self.rotation = random.random(self.theta1, self.theta2) * math.pi
-        """
-        self.random = random.random(0,1) * 0.5 + 0.25
-        print(self.random)
+        self.random = random.random(0,1) * (self.theta2 - self.theta1) + self.theta1
         self.rotation = self.random * math.pi
         self.vy = self.speed * math.sin(self.rotation)
         self.vx = self.speed * math.cos(self.rotation)
