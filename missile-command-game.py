0efaf4808d945992add764c8bc12af93a6965cc2
sth23 <47009471+sth23@users.noreply.github.com>
2019-05-09 20:15:58 -0400

    Updated from Brython Server: 5/9/2019 8:15:58 PM

--- missile-command-game.py
+++ missile-command-game.py
@@ -36,7 +36,8 @@ class MissileHead(Sprite):
         super().__init__(MissileHead.circ, (random.randint(0, width), 0))
         self.speed = speed
         self.fxcenter = self.fycenter = 0.5
-        self.rotation = math.pi / 2
+        self.rotation = random.random(0, math.pi)
+        print(self.rotation)
         self.vy = self.speed * math.sin(self.rotation)
         self.vx = -self.speed * math.cos(self.rotation)
         
